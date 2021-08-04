9fd324ec32b5ce04b5d0984e380b58cee8a0e9ec
hohohogugugu <bluehome8626@naver.com>
2021-08-04 17:49:36 +0900

    clone

--- dmsmeal.js
+++ dmsmeal.js
@@ -1,4 +1,5 @@
 let day = 28;
+
 function beforeday(){
     day--;
     apifetch();
@@ -10,14 +11,17 @@ function apifetch(){
       return response.json();
     })
     .then(function(myJson) {
-      console.log(JSON.stringify(myJson));
+      myJson[`2021-07-${day}`].breakfast.map(x=>breakfast(x));
     });
 }
 
 apifetch();
 
 const morning = document.getElementsByClassName("morning")[0];
-morning.innerHTML="<div> hello </div>" 
 
 const before = document.getElementsByClassName("before")[0];
 before.addEventListener("click",beforeday)
+
+function breakfast(menu){
+  morning.innerHTML+=`<div class="menucss"> ${menu} </div>`
+}
